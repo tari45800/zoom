bdfed045b99dda911102b91b4dd39508484713b8
tari45800 <tari45800@gmail.com>
2023-05-06 03:15:06 +0900

    feat:

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
-import express from "express"
+import express from "express";
+import http from "http";
+import WebSocket from 'ws';
 
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get("/*", (req,res) => res.redirect("/"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser")
+    socket.on("close", () => {
+        console.log("Disconnected from Browser")
+    })
+    socket.on("message", (message) => {
+        console.log(message.toString('utf-8'))
+    })
+    socket.send("hello!! ");
+});
 
 const handleListen = () => console.log('hello');
-
-app.listen(3000, handleListen);
+server.listen(3000, handleListen); 
