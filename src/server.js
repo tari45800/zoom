539f0487b306480088d070d764c3395a25ec5aeb
tari45800 <tari45800@gmail.com>
2023-05-10 23:42:03 +0900

    feat:

--- src/server.js
+++ src/server.js
@@ -13,18 +13,29 @@ app.get("/*", (_,res) => res.redirect("/"));
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-
 const sockets = []; 
 
 wss.on("connection", (socket) => {
-    sockets.push(socket);
-    console.log("Connected to Browser")
-    socket.on("close", () => {
-        console.log("Disconnected from Browser")
-    })
-    socket.on("message", (message) => {
-        sockets.forEach(aSocket => aSocket.send(message.toString('utf-8')))
-    })
+  sockets.push(socket);
+  socket["nickname"] = '알수없는'
+  console.log("Connected to Browser")
+  socket.on("close", () => {
+      console.log("Disconnected from Browser")
+  })
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg)
+
+    switch(message.type){
+      case "new_message":
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload}`));
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+
+    }
+
+  })
 });
 
 const handleListen = () => console.log('hello');
